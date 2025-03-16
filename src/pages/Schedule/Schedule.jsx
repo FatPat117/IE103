9b959bd630633937ec7678d6179e0cc33009e48a
Phat117 <ntphat110705@gmail.com>
2025-03-16 14:57:22 +0700

    Schedule #1

--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,5 +1,143 @@
+import React from "react";
+import "./Schedule.css";
 function Schedule() {
-        return <div class="schedule"></div>;
+        return (
+                <table className="table">
+                        <thead>
+                                <tr className="table__heading">
+                                        <th>Thứ/Tiết</th>
+                                        <th>Thứ 2</th>
+                                        <th>Thứ 3</th>
+                                        <th>Thứ 4</th>
+                                        <th>Thứ 5</th>
+                                        <th>Thứ 6</th>
+                                        <th>Thứ 7</th>
+                                </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                        <td>
+                                                Tiết 1 <br />
+                                                (7:30 - 8:15)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 2 <br />
+                                                (8:15 - 9:00)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 3 <br />
+                                                (9:00 - 9:45)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 4 <br />
+                                                (10:00 - 10:45)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 5 <br />
+                                                (10:45 - 11:30)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 6 <br />
+                                                (13:00 - 13:45)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 7 <br />
+                                                (13:45 - 14:30)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 8 <br />
+                                                (14:30 - 15:15)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 9 <br />
+                                                (15:30 - 16:15)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>
+                                                Tiết 10 <br />
+                                                (16:15 - 17:00)
+                                        </td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                </tr>
+                        </tbody>
+                </table>
+        );
 }
 
 export default Schedule;
