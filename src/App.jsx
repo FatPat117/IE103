9c957f10e462b945e0327bee0b0a0c96eb23814f
Phat117 <ntphat110705@gmail.com>
2025-04-06 15:02:42 +0700

    Commit truoc khi pull

--- src/App.jsx
+++ src/App.jsx
@@ -13,53 +13,44 @@ import Schedule from "./pages/Schedule/Schedule";
 import Login from "./pages/Login/Login";
 import Confirmed from "./pages/ConfirmedList/Confirmed";
 
-
 import AdminRoute from "./routes/AdminRoute";
 
 function App() {
-<<<<<<< HEAD
         const location = useLocation();
         const [userRole, setUserRole] = useState(localStorage.getItem("userRole"));
 
-        const location = useLocation();
-        const [userRole, setUserRole] = useState(localStorage.getItem("userRole"));
-=======
-    const location = useLocation();
-    const [userRole, setUserRole] = useState(localStorage.getItem("userRole"));
+        useEffect(() => {
+                setUserRole(localStorage.getItem("userRole"));
+        }, [location.pathname]);
 
-    useEffect(() => {
-        setUserRole(localStorage.getItem("userRole"));
-    }, [location.pathname]);
+        console.log("userRole:", userRole);
 
-    console.log("userRole:", userRole);
+        const isLoginPage = location.pathname === "/login";
+        const isAdminPage = location.pathname.startsWith("/admin");
 
-    const isLoginPage = location.pathname === "/login";
-    const isAdminPage = location.pathname.startsWith("/admin");
+        if (userRole === null && !isLoginPage) {
+                return <Navigate to="/login" replace />;
+        }
+        return (
+                <>
+                        {!isLoginPage && !isAdminPage && <Header />}
+                        <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/dashboard" element={<Home />} />
+                                <Route path="/dang-ky-hoc-phan" element={<ClassList />} />
+                                <Route path="/xac-nhan-hoc-phan" element={<Confirmed />} />
+                                <Route path="/danh-sach-lop" element={<Schedule />} />
+                                <Route path="/login" element={<Login />} />
 
-    if (userRole === null && !isLoginPage) {
-        return <Navigate to="/login" replace />;
-    }
-    return (
-        <>
-            {!isLoginPage && !isAdminPage && <Header />}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/dashboard" element={<Home />} />
-                <Route path="/dang-ky-hoc-phan" element={<ClassList />} />
-                <Route path="/xac-nhan-hoc-phan" element={<Confirmed />} />
-                <Route path="/danh-sach-lop" element={<Schedule />} />
-                <Route path="/login" element={<Login />} />
->>>>>>> f2c8b7d9c0ec1ef878d57c8ae4145cf2bb1553b7
-
-                <Route element={<AdminRoute />}>
-                    <Route path="/admin" element={<AdminDashboard />} />
-                    <Route path="/admin/subjects" element={<Subjects />} />
-                    <Route path="/admin/students" element={<Students />} />
-                </Route>
-            </Routes>
-            {!isLoginPage && !isAdminPage && <Footer />}
-        </>
-    );
+                                <Route element={<AdminRoute />}>
+                                        <Route path="/admin" element={<AdminDashboard />} />
+                                        <Route path="/admin/subjects" element={<Subjects />} />
+                                        <Route path="/admin/students" element={<Students />} />
+                                </Route>
+                        </Routes>
+                        {!isLoginPage && !isAdminPage && <Footer />}
+                </>
+        );
 }
 
 export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,68 +2,68 @@ import classNames from "classnames/bind";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo_header from "../../assets/image/logo_header.png";
-import styles from "../Login/Login.module.css";
 import { loginAPI } from "../../services/api.service";
+import styles from "../Login/Login.module.css";
 
 const cx = classNames.bind(styles);
 
 function Login() {
-    const [studentId, setStudentId] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+        const [studentId, setStudentId] = useState("");
+        const [password, setPassword] = useState("");
+        const navigate = useNavigate();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+        const handleLogin = async (e) => {
+                e.preventDefault();
 
-        try {
-            const res = await loginAPI(studentId, password, true);
+                try {
+                        const res = await loginAPI(studentId, password, true);
 
-            if (res) {
-                alert("Đăng nhập thành công!");
-                console.log(res);
+                        if (res) {
+                                alert("Đăng nhập thành công!");
+                                console.log(res);
 
-                localStorage.setItem("accessToken", res.accesstoken);
-                localStorage.setItem("refreshToken", res.refreshtoken);
-                navigate("/dashboard");
-                window.location.reload();
-            } else {
-                alert("Mã số sinh viên hoặc mật khẩu không đúng!");
-            }
-        } catch (error) {
-            alert("Lỗi đăng nhập: " + error.message);
-        }
-    };
+                                // localStorage.setItem("accessToken", res.accesstoken);
+                                // localStorage.setItem("refreshToken", res.refreshtoken);
+                                // navigate("/dashboard");
+                                // window.location.reload();
+                        } else {
+                                alert("Mã số sinh viên hoặc mật khẩu không đúng!");
+                        }
+                } catch (error) {
+                        alert("Lỗi đăng nhập: " + error.message);
+                }
+        };
 
-    return (
-        <div className={cx("wrapper")}>
-            <div className={cx("login-container")}>
-                <div className={cx("login-box")}>
-                    <div className={cx("login-logo")}>
-                        <img src={logo_header} alt="Logo UIT" className={cx("logo")} />
-                    </div>
-                    <form onSubmit={handleLogin}>
-                        <input
-                            type="text"
-                            placeholder="Mã số sinh viên"
-                            required
-                            value={studentId}
-                            onChange={(e) => setStudentId(e.target.value)}
-                        />
-                        <input
-                            type="password"
-                            placeholder="Mật khẩu"
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <button className={cx("login-btn")} type="submit">
-                            Đăng nhập
-                        </button>
-                    </form>
+        return (
+                <div className={cx("wrapper")}>
+                        <div className={cx("login-container")}>
+                                <div className={cx("login-box")}>
+                                        <div className={cx("login-logo")}>
+                                                <img src={logo_header} alt="Logo UIT" className={cx("logo")} />
+                                        </div>
+                                        <form onSubmit={handleLogin}>
+                                                <input
+                                                        type="text"
+                                                        placeholder="Mã số sinh viên"
+                                                        required
+                                                        value={studentId}
+                                                        onChange={(e) => setStudentId(e.target.value)}
+                                                />
+                                                <input
+                                                        type="password"
+                                                        placeholder="Mật khẩu"
+                                                        required
+                                                        value={password}
+                                                        onChange={(e) => setPassword(e.target.value)}
+                                                />
+                                                <button className={cx("login-btn")} type="submit">
+                                                        Đăng nhập
+                                                </button>
+                                        </form>
+                                </div>
+                        </div>
                 </div>
-            </div>
-        </div>
-    );
+        );
 }
 
 export default Login;
